eb284bcaf3562410cebc554e99e8c3aa9f446850
Artemvga <154210527+Artemvga@users.noreply.github.com>
2025-11-29 05:03:10 +0300

    Revert "Create DataMarkAR"
    This reverts commit 73103a9e223df8325c83124ee4c7b948ca73fa41.

--- ARproject/Assets/Scripts/ExitController.cs
+++ ARproject/Assets/Scripts/ExitController.cs
@@ -1,14 +1,9 @@
-// ExitController.cs
 using UnityEngine;
 
 public class ExitController : MonoBehaviour
 {
     public void Exit()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
         Application.Quit();
-#endif
     }
 }
